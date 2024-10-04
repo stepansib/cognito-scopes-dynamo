b946d6191a9bdcf891b1b08d8abfda0351b91eda
Stepan Yudin <stepan.sib@gmail.com>
2024-10-04 11:06:55 +0200

    16

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,6 +1,7 @@
 import { DeleteCommand, PutCommand, ScanCommand, ScanCommandOutput } from '@aws-sdk/lib-dynamodb';
+import { DescribeUserPoolClientCommand } from '@aws-sdk/client-cognito-identity-provider';
 import { AllowedScopesPolicy, AllowedScopesPolicyRecordKey } from './types';
-import { dynamoDocumentClient } from './utils/aws-clients';
+import { cognitoClient, dynamoDocumentClient } from './utils/aws-clients';
 
 export const handler = async (): Promise<void> => {
   // Erase client scopes config
@@ -21,12 +22,18 @@ export const handler = async (): Promise<void> => {
   await Promise.all(cleanupPromises);
 
   // Setup client scopes config
-  // Todo: populate scopes from app client config instead of hardcoded array
+
+  const describeUserPoolClientCommand = new DescribeUserPoolClientCommand({
+    UserPoolId: process.env.USER_POOL_ID,
+    ClientId: process.env.APP_CLIENT_ID,
+  });
+  const userPoolClient = (await cognitoClient.send(describeUserPoolClientCommand)).UserPoolClient;
+
   const putCommand = new PutCommand({
     TableName: process.env.CLIENT_SCOPES_TABLE,
     Item: {
       client: process.env.APP_CLIENT_ID,
-      scopes: ['account/read', 'account/write'],
+      scopes: userPoolClient.AllowedOAuthScopes,
     } satisfies AllowedScopesPolicy,
   });
   await dynamoDocumentClient.send(putCommand);
--- src/utils/aws-clients.ts
+++ src/utils/aws-clients.ts
@@ -1,10 +1,12 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
+import { CognitoIdentityProviderClient } from '@aws-sdk/client-cognito-identity-provider';
 
 const clientOptions = {
   region: process.env.AWS_REGION,
 };
 const dynamoClient = new DynamoDBClient(clientOptions);
 const dynamoDocumentClient = DynamoDBDocumentClient.from(dynamoClient);
+const cognitoClient = new CognitoIdentityProviderClient(clientOptions);
 
-export { clientOptions, dynamoDocumentClient };
+export { clientOptions, dynamoDocumentClient, cognitoClient };
